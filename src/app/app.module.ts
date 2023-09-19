cc268ea98bb480746852af64bf962428f0354124
Anthony Erazo <anthonyerazo76@gmail.com>
2023-09-18 21:55:58 -0500

    agregue toolbar,sidebar y el layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LayoutComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
